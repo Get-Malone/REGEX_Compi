9f73822db24d9b4218d7bc5df012bd9f420638b5
Jose Armando Lopez Alvarez <60891410+JS2202@users.noreply.github.com>
2022-06-16 09:43:39 -0500

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ function submitFunction() {
     const username = document.getElementById('username');
     const email = document.getElementById('email');
     const ip = document.getElementById('ip');
+    const regex = document.getElementById('regex');
+    const regexing = document.getElementById('regexin');
 
     const re = /^((?=\S*?[A-Z])(?=\S*?[a-z])(?=\S*?[0-9]).{6,})\S$/;
     const re2 = /^(?=.*[^\.]$)((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.?){4}$/gmi;
@@ -25,6 +27,11 @@ function submitFunction() {
     }
 
     if(!re3.test(email.value)) {
+        window.alert(email.value + " No es valido");
+    } else {
+        window.alert(email.value + " Es valido");
+    }
+    if(!regex.test(regexin.value)) {
         window.alert(email.value + " No es valido");
     } else {
         window.alert(email.value + " Es valido");
