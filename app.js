b210a5faf5a1b2ec36e9c9d50d338dbb396d679d
Jose Armando Lopez Alvarez <60891410+JS2202@users.noreply.github.com>
2022-06-16 10:25:03 -0500

    Update app.js

--- app.js
+++ app.js
@@ -7,12 +7,14 @@ function submitFunction() {
     const username = document.getElementById('username');
     const email = document.getElementById('email');
     const ip = document.getElementById('ip');
-    const regex = document.getElementById('regex');
-    const regexing = document.getElementById('regexin');
+    const rfc = document.getElementById('rfc');
+    const  num = document.getElementById('rfc');
 
     const re = /^((?=\S*?[A-Z])(?=\S*?[a-z])(?=\S*?[0-9]).{6,})\S$/;
     const re2 = /^(?=.*[^\.]$)((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.?){4}$/gmi;
     const re3 = /^([a-z0-9_\.-]+\@[\da-z\.-]+\.[a-z\.]{2,6})$/gm;
+    const re4 = /[a-zA-Z]{1,4}[0-9]{1,6}[a-zA-Z0-9]{1,3}/
+    const re5 = /^[0-9\+]{1,}[0-9\-]{3,15}$/
 
     if(!re.test(username.value)) {
         window.alert(username.value + "No es valido");
@@ -31,9 +33,14 @@ function submitFunction() {
     } else {
         window.alert(email.value + " Es valido");
     }
-    if(!regex.test(regexin.value)) {
-        window.alert(email.value + " No es valido");
+    if(!re4.test(rfc.value)) {
+        window.alert(rfc.value + " No es valido");
     } else {
-        window.alert(email.value + " Es valido");
+        window.alert(rfc.value + " Es valido");
+    }
+    if(!re4.test(num.value)) {
+        window.alert(num.value + " No es valido");
+    } else {
+        window.alert(num.value + " Es valido");
     }
 }
